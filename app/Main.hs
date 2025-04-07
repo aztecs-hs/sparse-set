968ff5b46b15cefad394311f0c7708742904c82b
Matt Hunzinger <matthunz2@gmail.com>
2025-04-07 19:02:55 -0400

    feat: intersection

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,7 @@ module Main where
 import qualified Data.SparseSet as S
 
 main :: IO ()
-main = print . S.delete 10 . S.insert (10 :: Int) "B" $ S.insert 0 "A" S.empty
+main = print $ S.intersection as bs
+  where
+    as = S.insert (10 :: Int) "B" $ S.insert 0 "A" S.empty
+    bs = S.insert (10 :: Int) "B" S.empty
--- src/Data/SparseSet.hs
+++ src/Data/SparseSet.hs
@@ -1,10 +1,35 @@
-module Data.SparseSet where
+-- |
+-- Module      : Data.SparseSet
+-- Copyright   : (c) Matt Hunzinger, 2025
+-- License     : BSD-style (see the LICENSE file in the distribution)
+--
+-- Maintainer  : matt@hunzinger.me
+-- Stability   : provisional
+-- Portability : non-portable (GHC extensions)
+module Data.SparseSet
+  ( -- * Sparse sets
+    SparseSet (..),
+
+    -- ** Construction
+    empty,
+
+    -- ** Operations
+    insert,
+    lookup,
+    delete,
+
+    -- ** Intersection
+    intersection,
+    intersectionWith,
+  )
+where
 
 import Data.SparseVector (SparseVector)
 import qualified Data.SparseVector as SV
 import Data.Vector (Vector)
 import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as MV
+import Prelude hiding (lookup)
 
 data SparseSet i a = SparseSet
   { dense :: Vector a,
@@ -14,6 +39,7 @@ data SparseSet i a = SparseSet
 
 empty :: SparseSet i a
 empty = SparseSet V.empty SV.empty
+{-# INLINE empty #-}
 
 insert :: (Integral i) => i -> a -> SparseSet i a -> SparseSet i a
 insert i a s =
@@ -25,12 +51,14 @@ insert i a s =
         { dense = V.snoc (dense s) a,
           sparse = SV.insert (fromIntegral i) (fromIntegral $ V.length $ dense s) (sparse s)
         }
+{-# INLINE insert #-}
 
 lookup :: (Integral i) => SparseSet i a -> i -> Maybe a
 lookup s i =
   case SV.lookup (fromIntegral i) $ sparse s of
     Just denseIndex -> Just $ dense s V.! fromIntegral denseIndex
     Nothing -> Nothing
+{-# INLINE lookup #-}
 
 delete :: (Integral i) => i -> SparseSet i a -> SparseSet i a
 delete i s =
@@ -43,3 +71,31 @@ delete i s =
           sparse = SV.delete (fromIntegral i) (sparse s)
         }
     Nothing -> s
+{-# INLINE delete #-}
+
+intersection ::
+  (Integral i) =>
+  SparseSet i a ->
+  SparseSet i b ->
+  SparseSet i a
+intersection as bs =
+  let x = SV.intersection (sparse as) (sparse bs)
+      (_, x') = SV.mapAccum (\i _ -> (i + 1, i)) 0 x
+      as' = V.map (\i -> dense as V.! fromIntegral i) (SV.toVector x)
+   in SparseSet {dense = as', sparse = x'}
+{-# INLINE intersection #-}
+
+intersectionWith ::
+  (Integral i) =>
+  (a -> b -> c) ->
+  SparseSet i a ->
+  SparseSet i b ->
+  SparseSet i c
+intersectionWith f as bs =
+  let x = SV.intersection (sparse as) (sparse bs)
+      (_, x') = SV.mapAccum (\i _ -> (i + 1, i)) 0 x
+      as' = V.map (\i -> dense as V.! fromIntegral i) (SV.toVector x)
+      bs' = V.map (\i -> dense bs V.! fromIntegral i) (SV.toVector x)
+      cs = V.zipWith f as' bs'
+   in SparseSet {dense = cs, sparse = x'}
+{-# INLINE intersectionWith #-}
